b46bc4f095794e10449f5af0d9d534217572adf8
sheethappenshq <sheethappenshq@gmail.com>
2025-09-24 12:17:32 +1000

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Sheet-Happens-Site/', // 👈 important: must match your repo name
-})
